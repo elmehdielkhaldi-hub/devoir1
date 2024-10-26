3ff21538f9f550d18ccfb24301c28e1b5c243b32
elmehdielkhaldi <elkhaldi.elmehdi@etu.uae.ac.ma>
2024-10-27 00:57:56 +0100

    Update classcar.cpp

--- classcar.cpp
+++ classcar.cpp
@@ -1,60 +1,66 @@
-  #include<iostream>
-  using namespace std ;
-  class voiture{
-  private :
-  string marque;
-  string modele;
-  int annee;
-  float km;
-  float vitesse ;
+#include<iostream>
+#include<string>
+using namespace std;
+class voiture {
+    private:
+   string marque ;
+   string modele ;
+   int annee;
+   float kilometrage;
+   float vitesse;
+    public:
+    //prototype
+      voiture();
+        voiture(string mar , string mo , int an , float kil , float vi);
+        void accelerer(float v);
+        double freiner(float valeur);
+        void afficherinfo();
+        void avance(float distance);
+        ~voiture();
+};
+   voiture::voiture() : marque("honda"),modele("civik"),annee(2081),kilometrage(20000),vitesse(120){}
+ voiture ::voiture(string ma,string mo,int year,float km ,float speed) : marque(ma),modele(mo),annee(year),kilometrage(km),vitesse(speed){}
+  
+  
+  void voiture::accelerer (float v){
+     vitesse =vitesse+v;
+     cout<<"acceleration est >>>>>>>>>"<<vitesse<<endl;
+  }
+  double voiture ::freiner (float valeur){
+    vitesse=vitesse-valeur;
+    if(vitesse>0){
+    return vitesse ;
+    }else 
+    return 0;
+      }
+      
+  void voiture::afficherinfo()
+{
+    cout<<"-------- information sur la voiture-----"<<endl;
+    cout<<"la marque est "<<marque<<endl;
+    cout<<"le modele est"<<modele<< endl ;
+    cout<<"annee de fabrication est "<<annee<< endl;
+    cout<<"kilometrage actuelle de la voiture est "<<kilometrage<< endl;
+    cout<<"vitesse actuelle de la voiture "<< vitesse<< endl;
+}
+void voiture::avance(float distance){
+         kilometrage=kilometrage + distance;
+         cout<<"kilometrage est  :"<< kilometrage<<endl;
+      }
+voiture ::~voiture(){
+    cout<<"la voiture est detruite"<<endl;
+}
 
-  public:
-  
-  voiture(); //constructer par defaut
-  voiture(string marqu,string model,int anne,float kilom,float vitessee);// constructer with arguments
-    void accelerer (float num);
-    double freiner(float num);
-    void affichinfo();
-    void avancer(float distance);
-  ~voiture();
-  };  
-     voiture::voiture():marque("bmw"),modele("serie 4"),annee(2015),km(20000.0), vitesse(220.0){}
-     voiture::voiture(string marqu , string model, int anne , float kilom , float vitessee): marque (marqu) , modele(model) , annee(anne) , km(kilom) , vitesse(vitessee){}
-  
-  void voiture::accelerer(float num){
-    vitesse = vitesse+num;
-  }
-  double voiture::freiner(float num){
-    if(num>0){
-    vitesse=vitesse - num ;
-    return vitesse;
-    } 
-    else 
-    vitesse=num;
-    
-  }
-  void voiture :: affichinfo(){
-    cout<<"detail de la voiture"<<endl;
-    cout << " la marque est:         "<<marque<<endl;
-    cout<<"le modele de la voiture est:  "<<modele<<endl;
-    cout<<"l annee de fabrication est:   "<<annee<<endl;
-    cout<<"le kilométrage actuel de la voiture:    "<< km<<endl;
-    cout <<"la vitesse actuelle de la voiture:    "<< vitesse<< endl;
-  }
-   void voiture :: avancer(float distance){
-    km = km +distance;
+
+int main (){
+    voiture v1;
+    v1.afficherinfo();
+voiture ob1("honda","serie 4",33,999,0);
+v1.accelerer(100);
+v1.freiner(50);
+ v1.avance(1000);
+
+ob1.afficherinfo();
+
+
 }
-voiture::~voiture(){ // destructer
-    cout<<"la voiture est detruite"<<endl; 
-}
-  int main(){
-voiture car1;
-car1.affichinfo();
-voiture v2("dacia", "sandero", 2018, 1000000, 150);
-    
-    v2.accelerer(50);  
-    cout <<"la vitesse au freinage est"<< v2.freiner(50)<<endl;    
-    v2.avancer(200);
-    v2.affichinfo();
-    return 0 ;
-  }
